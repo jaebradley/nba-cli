8855c4bc87a14260d87e202b4706dcc0b2f1d7e1
jaebradley <jae.b.bradley@gmail.com>
2016-05-06 00:28:48 -0400

    simplified table building

--- src/commandLineOutputClient.js
+++ src/commandLineOutputClient.js
@@ -1,108 +1,29 @@
-const Table = require('cli-table');
+const Table = require('cli-table2');
 const NbaDataClient = require('./nbaDataClient.js');
 const moment = require('moment-timezone');
-
-const defaultTableFormatting = {
-  chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
-         , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
-         , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
-         , 'right': '║' , 'right-mid': '╢' , 'middle': '│' }
-};
+const StartedGameTableCreator = require('./tables/StartedGameTableCreator.js');
+const UpcomingGameTableCreator = require('./tables/UpcomingGameTableCreator.js');
 
 function outputGames(data) {
+  const upcomingGameData = [];
   Object.keys(data).forEach(function(key) {
     const gameData = data[key];
     if (isGameUpcoming(gameData)) {
-      outputUpcomingGameTable(gameData);
+      upcomingGameData.push(gameData);
     } else {
-      outputStartedGameTable(gameData);
+      console.log(StartedGameTableCreator.createStartedGameTable(gameData));
     }
   });
-}
 
-function outputUpcomingGameTable(data) {
-  const table = new Table(defaultTableFormatting);
-  const location = data.arena.concat(',', data.city, ',', data.state);
-  table.push(
-    ['HOME', 'AWAY', 'START TIME', 'WATCH IT ON', 'ARENA'],
-    [data.homeName, data.visitorName, data.formattedLocalizedStartDate, data.broadcasts.toString(), location]
-  );
-  console.log(table.toString());
+  if (upcomingGameData.length > 0) {
+    console.log(UpcomingGameTableCreator.createUpcomingGamesTable(upcomingGameData));
+  }
 }
 
 function isGameUpcoming(data) {
   return data.unixMillisecondsStartTime > moment().valueOf();
 }
 
-function outputStartedGameTable(data) {
-  outputLinescoresTable(data);
-  outputUpcomingGameTable(data);
-}
-
-function outputLinescoresTable(data) {
-  const table = new Table(defaultTableFormatting);
-  const headers = generateLinescoresTableHeaders(data.homeLinescores);
-  const homeRow = generateTeamLinescoresTableRow(data.homeAbbreviation, data.homeLinescores, data.homeScore);
-  const visitorRow = generateTeamLinescoresTableRow(data.visitorAbbreviation, data.visitorLinescores, data.visitorScore);
-  homeRow.unshift(generateFormattedPeriodValue(data.periodValue));
-  visitorRow.unshift(generateGameStatus(data.status, data.periodValue, data.gameClock));
-  table.push(
-    headers,
-    homeRow,
-    visitorRow
-  );
-  console.log(table.toString());
-}
-
-function generateLinescoresTableHeaders(linescores) {
-  const headers = ['', ''];
-  linescores.forEach(function(linescore) {
-    headers.push(linescore.period);
-  });
-  headers.push('Total');
-  return headers;
-}
-
-function generateTeamLinescoresTableRow(teamAbbreviation, linescores, total) {
-  const row = [teamAbbreviation];
-  linescores.forEach(function(linescore) {
-    row.push(linescore.score);
-  });
-  row.push(total);
-  return row;
-}
-
-function generateFormattedPeriodValue(periodValue) {
-  if (periodValue == "" || parseInt(periodValue) <= 1) {
-    return 'Q1';
-  }
-
-  else if (parseInt(periodValue) > 4) {
-    return 'OT'.concat(periodValue - 4);
-  }
-
-  return 'Q'.concat(periodValue);
-}
-
-function generateGameStatus(status, periodValue, gameClock) {
-  if (status == "LIVE") {
-    return generateFormattedGameClock(periodValue, gameClock);
-  }
-
-  return status;
-}
-
-function generateFormattedGameClock(periodValue, gameClock) {
-  if ((typeof periodValue == "string" || parseInt(periodValue) <= 1) && gameClock == '') {
-    return '12:00';
-  } 
-
-  else if (parseInt(periodValue) > 4 && gameClock == '') {
-    return '05:00';
-  }
-
-  return gameClock;
-}
 
 module.exports = {
   outputTodayGames: function() {
@@ -115,5 +36,9 @@ module.exports = {
 
   outputTomorrowGames: function() {
     NbaDataClient.fetchTomorrowGames(outputGames);
+  },
+
+  outputCustomDateGames: function(date) {
+    NbaDataClient.fetchCustomDateGames(date, outputGames);
   }
 };
