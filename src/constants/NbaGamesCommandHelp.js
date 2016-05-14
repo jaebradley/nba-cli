ef914fad252604045b71f363c8c491677a01db9a
jaebradley <jae.b.bradley@gmail.com>
2016-05-14 14:37:30 -0400

    first es6 class

--- src/constants/NbaGamesCommandHelp.js
+++ src/constants/NbaGamesCommandHelp.js
@@ -4,7 +4,4 @@ const USAGE = '\nUsage: nba [options] [commands] \n\n';
 const COMMANDS = 'Commands: \n\n  games [time] get nba games for date \n\n';
 const OPTIONS = 'Options: \n\n  -h, --help  output usage information \n\n';
 const EXAMPLES = 'Examples: \n\n nba games today => get games that started today \n\n nba games tomorrow => get games starting tomorrow \n\n nba games yesterday => get games that started yesterday \n\n nba games 2016-01-01 => nba games that started on 2016-01-01 \n';
-
-module.exports = {
-  TEXT: USAGE.concat(COMMANDS, OPTIONS, EXAMPLES),
-};
+export const HELP = `${USAGE}${COMMANDS}${OPTIONS}${EXAMPLES}`;
