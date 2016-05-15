9047d0a0799ae0bbb8be9cbe24cf2800fce2ed8b
jaebradley <jae.b.bradley@gmail.com>
2016-05-15 10:45:45 -0400

    minor fixes

--- src/tables/UpcomingGameTableCreator.js
+++ src/tables/UpcomingGameTableCreator.js
@@ -8,13 +8,13 @@ export default class UpcomingGameTableCreator {
 
   create(upcomingGamesData) {
     const table = new Table(this.defaultFormat);
-    data.map(upcomingGame => (
+    upcomingGamesData.map(upcomingGame => (
       table.push([
         upcomingGame.formattedLocalizedStartDate,
         upcomingGame.homeName,
         upcomingGame.visitorName,
         upcomingGame.broadcasts.toString(),
-        UpcomingGameTableCreator.getLocation(upcomingGame.arena, upcomingGame.city, upcomingGame.state)
+        UpcomingGameTableCreator.formatLocation(upcomingGame.arena, upcomingGame.city, upcomingGame.state)
       ])
     ));
     return table.toString();
