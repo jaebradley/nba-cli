338cbf2ab0be1c883548d4828457ec5fac2fc27e
jaebradley <jae.b.bradley@gmail.com>
2016-05-18 00:52:43 -0400

    added statistical leaders test

--- src/data/models/StatisticalLeaders.js
+++ src/data/models/StatisticalLeaders.js
@@ -2,7 +2,7 @@ import {Record} from 'immutable';
 
 const defaults = {
   value: 0,
-  leaders: []
+  leaders: [],
 };
 
 export default class StatisticalLeaders extends Record(defaults) {
---
+++ test/StatisticalLeaders.js
@@ -0,0 +1,22 @@
+import {expect} from 'chai';
+import StatisticalLeaders from '../src/data/models/StatisticalLeaders';
+import Player from '../src/data/models/Player';
+
+describe('Statistical Leaders object', function() {
+  it('creates a statistical leaders object', function() {
+    const emptyStatisticalLeaders = new StatisticalLeaders();
+
+    expect(emptyStatisticalLeaders.value).to.equal(0);
+    expect(emptyStatisticalLeaders.leaders.length).to.equal(0);
+    expect(emptyStatisticalLeaders.getLeadersAbbreviatedNames()).to.equal('');
+
+    const me = new Player({firstName: 'jae', lastName: 'bradley'});
+    const players = [me, me, me];
+    const expectedAbbreviatedNames = [me.getAbbreviatedName(), me.getAbbreviatedName(), me.getAbbreviatedName()].toString();
+    const meStatisticalLeaders = new StatisticalLeaders({value: 5, leaders: players});
+
+    expect(meStatisticalLeaders.value).to.equal(5);
+    expect(meStatisticalLeaders.leaders).to.equal(players);
+    expect(meStatisticalLeaders.getLeadersAbbreviatedNames()).to.equal(expectedAbbreviatedNames);
+  });
+});
