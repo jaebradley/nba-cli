a44f5fe3ce6ce30d58293ffd054e73234e1d4841
jaebradley <jae.b.bradley@gmail.com>
2016-05-23 00:31:14 -0400

    added scoreboard tests

--- test/ScoreboardDataTranslator.js
+++ test/ScoreboardDataTranslator.js
@@ -3,6 +3,8 @@
 import {expect} from 'chai';
 import ScoreboardDataTranslator from '../src/translators/data/ScoreboardDataTranslator';
 
+import firstQuarterScoreboard from './data/scoreboard/first-quarter.json';
+
 describe('Translate scoreboard data', function() {
 
   it('Indicates if recap is available', function() {
@@ -19,5 +21,19 @@ describe('Translate scoreboard data', function() {
     expect(ScoreboardDataTranslator.isPreviewAvailable(2)).to.equal(false);
   });
 
-  
+  it('Calculates unix millisecond start time', function() {
+    expect(ScoreboardDataTranslator.getUnixMillisecondsStartTime('201512311900'), 1451606400000); // 2016-01-01 00:00:00
+  });
+
+  it('Identifies game status', function() {
+    expect(ScoreboardDataTranslator.getGameStatus('Halftime', 1)).to.equal('Halftime');
+    expect(ScoreboardDataTranslator.getGameStatus('Jae', 1)).to.equal('PREGAME');
+    expect(ScoreboardDataTranslator.getGameStatus('Jae', 2)).to.equal('LIVE');
+    expect(ScoreboardDataTranslator.getGameStatus('Jae', 3)).to.equal('FINAL');
+  });
+
+  it('Identifies tv broadcasts', function() {
+    const broadcasters = firstQuarterScoreboard.sports_content.games.game[0].broadcasters;
+    expect(ScoreboardDataTranslator.getBroadcasts(broadcasters)).to.eql(['TNT', 'Sportsnet One']);
+  })
 });
