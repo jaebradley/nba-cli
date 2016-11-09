cfe49022b764fcd769278dbe3d251786609ab077
jbradley <jbradley@hubspot.com>
2016-11-09 16:56:57 -0500

    update player

--- src/data/models/Player.js
+++ src/data/models/Player.js
@@ -1,11 +1,21 @@
 import {Record} from 'immutable';
 
-const defaults = {
-  firstName: "",
-  lastName: "",
-};
+export default class Player extends Record {
+  constructor(firstName, lastName) {
+    if (typeof firstName !== "string") {
+      throw new Error("player first name is not a string");
+    }
 
-export default class Player extends Record(defaults) {
+    if (typeof lastName !== "string") {
+      throw new Error("player last name is not a string");
+    }
+
+    super({
+      firstName: firstName,
+      lastName: lastName
+    });
+  }
+
   getAbbreviatedFirstName() {
     if (this.firstName.length > 0) {
       return this.firstName.charAt(0);
