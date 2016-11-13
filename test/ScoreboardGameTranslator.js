410174c24c01d02acb1dd0837d9df0e47bcdc523
jaebradley <jae.b.bradley@gmail.com>
2016-11-13 14:56:28 -0500

    fixing tests

--- test/ScoreboardGameTranslator.js
+++ test/ScoreboardGameTranslator.js
@@ -1,7 +1,10 @@
 'use es6';
 
 import {expect, assert} from 'chai';
+import {List} from 'immutable';
 
+import Broadcast from '../src/data/models/Broadcast';
+import BroadcastMedium from '../src/data/models/BroadcastMedium';
 import GameStatus from '../src/data/models/GameStatus';
 import Location from '../src/data/models/Location';
 import Period from '../src/data/models/Period';
@@ -11,6 +14,7 @@ import finalGame from './data/scoreboard/final-game';
 
 describe('translate scoreboard game', function() {
   let periodTime = finalGame.period_time;
+  let broadcasters = finalGame.broadcasters;
   it('tests getting game status', function() {
     expect(ScoreboardGameTranslator.getGameStatus(periodTime)).to.equal(GameStatus.FINAL);
   });
@@ -31,5 +35,15 @@ describe('translate scoreboard game', function() {
   it('tests getting period', function() {
     let expectedPeriod = new Period(4, 'Final', '');
     expect(ScoreboardGameTranslator.getPeriod(periodTime)).to.eql(expectedPeriod);
+  });
+
+  it('tests getting broadcasts', function() {
+    let expectedBroadcasts = new List.of(
+      new BroadCast('local', 'WTAM 1100 / 87.7 FM (ESP)', BroadcastMedium.RADIO),
+      new BroadCast('local', '92.9 FM \"The Game\"', BroadcastMedium.RADIO),
+      new BroadCast('natl', 'ABC', BroadcastMedium.TV),
+      new BroadCast('can', 'TSN', BroadcastMedium.TV),
+    );
+    expect(ScoreboardGameTranslator.getBroadcasts(broadcasters)).to.eql(expectedBroadcasts);
   });
 });
