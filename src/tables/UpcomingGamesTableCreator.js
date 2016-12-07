d0ea8f7faa9a9239e1739d4df7ff34b35df5f11a
jaebradley <jae.b.bradley@gmail.com>
2016-12-06 20:14:22 -0500

    start with updated game table creators

---
+++ src/tables/UpcomingGamesTableCreator.js
@@ -0,0 +1,31 @@
+import Table from 'cli-table2';
+import {List} from 'immutable';
+
+export default class UpcomingGamesTableCreator {
+
+  static create(data) {
+    let table = new Table(UpcomingGameTableCreator.getTableConfiguration());
+    data.forEach(metadata => table.push(UpcomingGamesTableCreator.formatMetadata(metadata)));
+    return table.toString();
+  }
+
+  static formatMetadata(metadata) {
+    return List.of(
+      metadata.getLocalizedStartDateTime(),
+      metadata.home.getName(),
+      metadata.visitor.getName(),
+      metadata.getBroadcastsString(),
+      metadata.location.getFormattedLocation(),
+    );
+  }
+
+  static getTableConfiguration() {
+    return {
+      head: UpcomingGameTableCreator.getHeader().toJS()
+    }
+  }
+
+  static getHeader() {
+    return List.of('UPCOMING', 'HOME', 'AWAY', 'WATCH', 'ARENA');
+  }
+}
