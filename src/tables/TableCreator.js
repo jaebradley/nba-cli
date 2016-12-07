65a855e8caeee7d358d1daf284ecaf9035ffbae0
jaebradley <jae.b.bradley@gmail.com>
2016-12-06 22:55:38 -0500

    implement table creator

--- src/tables/TableCreator.js
+++ src/tables/TableCreator.js
@@ -6,6 +6,24 @@ import UpcomingGamesTableCreator from './UpcomingGamesTableCreator';
 
 export default class TableCreator {
   static create(data) {
+    console.log(TableCreator.createActiveGamesTables(data.active));
+    console.log(TableCreator.createActiveGamesTables(data.upcoming));
+  }
 
+  static createUpcomingGamesTable(games) {
+    return UpcomingGamesTableCreator.create(games);
+  }
+
+  static createActiveGamesTables(games) {
+    let table = new Table();
+    table.push([
+      ActiveGameTableCreator.create(games),
+      PlayByPlayTableCreator.create(games.playByPlay)
+    ]);
+    table.push([
+      BoxScoreTableCreator.create(games.boxScoreLeaders.home),
+      BoxScoreTableCreator.create(games.boxScoreLeaders.visitor)
+    ]);
+    return table.toString();
   }
 }
