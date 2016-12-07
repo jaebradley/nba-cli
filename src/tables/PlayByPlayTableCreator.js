1310e681d64fd034f9cb287c7f70e41392485b67
jaebradley <jae.b.bradley@gmail.com>
2016-12-06 20:17:30 -0500

    update pbp table creator

--- src/tables/PlayByPlayTableCreator.js
+++ src/tables/PlayByPlayTableCreator.js
@@ -5,20 +5,26 @@ import Formatter from './formatters/Formatter';
 
 export default class PlayByPlayTableCreator {
 
-  static create(playByPlayData)  {
-    let table = new Table({ head: PlayByPlayTableCreator.getHeader() });
-    playByPlayData.forEach(play => table.push(PlayByPlayTableCreator.generateRow(play)));
+  static create(data)  {
+    let table = new Table(PlayByPlayTableCreator.getTableConfiguration());
+    data.forEach(play => table.push(PlayByPlayTableCreator.generateRow(play)));
     return table.toString();
   }
 
-  static generateFormattedPlayClock(playClock, playPeriod) {
-    let formattedGamePeriod = Formatter.formatLiveGamePeriod(playPeriod);
-    return `${playClock} ${formattedGamePeriod}`;
+  static generateFormattedPlayClock(clock, period) {
+    let formattedPeriod = Formatter.formatLiveGamePeriod(period);
+    return `${clock} ${formattedPeriod}`;
   }
 
   static generateRow(play) {
     let playClock = PlayByPlayTableCreator.generateFormattedPlayClock(play.clock, play.period);
     return List.of(playClock, play.description);
+  }
+
+  static getTableConfiguration() {
+    return {
+      head: PlayByPlayTableCreator.getHeader().toJS()
+    };
   }
 
   static getHeader() {
