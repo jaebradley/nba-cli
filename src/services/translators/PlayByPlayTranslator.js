8a4b87dbd7abe1978c7bb9c31b17cf797f42d5b1
jaebradley <jae.b.bradley@gmail.com>
2016-12-25 17:55:21 -0500

    update play by play translator

--- src/services/translators/PlayByPlayTranslator.js
+++ src/services/translators/PlayByPlayTranslator.js
@@ -1,17 +1,17 @@
 import {List} from 'immutable';
 
-import PlayByPlay from '../../data/PlayByPlay';
+import Play from '../../data/Play';
 
 export default class PlayByPlayTranslator {
   static translate(data) {
-    let playByPlayData = data.sports_content.game.play;
-    let index = Math.min(5, playByPlayData.length);
-    let recentPlays = playByPlayData.slice(-index);
-    return List(recentPlays.map(play => PlayByPlayTranslator.buildPlayByPlay(play)));
+    let plays = data.sports_content.game.play;
+    let index = Math.min(5, plays.length);
+    let recentPlays = plays.slice(-index);
+    return List(recentPlays.map(play => PlayByPlayTranslator.Play(play)));
   }
 
-  static buildPlayByPlay(play) {
-    return new PlayByPlay({
+  static buildPlay(play) {
+    return new Play({
       description: play.description,
       clock: play.clock,
       period: parseInt(play.period),
