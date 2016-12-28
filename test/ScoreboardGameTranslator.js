66fff58d6928280cee798aad096ee38282cc8434
jaebradley <jae.b.bradley@gmail.com>
2016-12-28 12:16:10 -0500

    add period score test

--- test/ScoreboardGameTranslator.js
+++ test/ScoreboardGameTranslator.js
@@ -9,6 +9,7 @@ import BroadcastMedium from '../src/data/BroadcastMedium';
 import GameStatus from '../src/data/GameStatus';
 import Location from '../src/data/Location';
 import Period from '../src/data/Period';
+import PeriodScore from '../src/data/PeriodScore';
 import Matchup from '../src/data/Matchup';
 import Team from '../src/data/Team';
 import Score from '../src/data/Score';
@@ -23,19 +24,42 @@ describe('translate scoreboard game', function() {
   let awayScore1Value = '2';
   let homeScore1 = 1;
   let awayScore1 = 2;
-  let homeData = {
-    'score': homeScore1Value
-  };
-  let awayData = {
-    'score': awayScore1Value
-  };
+
+  let period1Value = '3';
+  let anotherPeriod1Value = '4';
+  let period1 = 3;
+  let anotherPeriod1 = 4;
+  let score1 = new Score({
+    home: homeScore1,
+    away: awayScore1
+  });
 
   it('should test total score parsing', function() {
-    let expected = new Score({
-      home: homeScore1,
-      away: awayScore1
-    });
+    let homeData = {
+      'score': homeScore1Value
+    };
+    let awayData = {
+      'score': awayScore1Value
+    };
+
     let totalScore = ScoreboardGameTranslator.getTotalScore(homeData, awayData)
-    expect(totalScore).to.eql(expected);
+    expect(totalScore).to.eql(score1);
+  });
+
+  it('should test period score parsing', function() {
+    let homePeriodScore = {
+      'period_value': period1Value,
+      'score': homeScore1
+    };
+    let awayPeriodScore = {
+      'period_value': period1Value,
+      'score': awayScore1
+    };
+    let periodScore1 = new PeriodScore({
+      period: period1,
+      score: score1
+    });
+    let periodScore = ScoreboardGameTranslator.getPeriodScore(homePeriodScore, awayPeriodScore);
+    expect(periodScore).to.eql(periodScore1);
   });
 });
