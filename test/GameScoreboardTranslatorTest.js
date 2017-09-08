205f424721b83ad4f1179365329eff6b334ddc90
jaebradley <jae.b.bradley@gmail.com>
2017-09-08 10:35:47 -0400

    add broadcasters test

--- test/GameScoreboardTranslatorTest.js
+++ test/GameScoreboardTranslatorTest.js
@@ -1,6 +1,6 @@
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
-import {List} from 'immutable';
+import { List } from 'immutable';
 
 import Broadcast from '../src/data/Broadcast';
 import BroadcastMedium from '../src/data/BroadcastMedium';
@@ -15,36 +15,36 @@ import GameScoreboardTranslator from '../src/services/translators/GameScoreboard
 
 chai.use(chaiImmutable);
 
-let expect = chai.expect;
+const expect = chai.expect;
 
-describe('translate game scoreboards', function() {
-  let homeScore1Value = '1';
-  let awayScore1Value = '2';
-  let homeScore1 = 1;
-  let awayScore1 = 2;
+describe('translate game scoreboard', function() {
+  const homeScore1Value = '1';
+  const awayScore1Value = '2';
+  const homeScore1 = 1;
+  const awayScore1 = 2;
 
-  let period1Value = '3';
-  let anotherPeriod1Value = '4';
-  let period1 = 3;
-  let anotherPeriod1 = 4;
-  let score1 = new Score({
+  const period1Value = '3';
+  const anotherPeriod1Value = '4';
+  const period1 = 3;
+  const anotherPeriod1 = 4;
+  const score1 = new Score({
     home: homeScore1,
     away: awayScore1
   });
-  let homePeriodScore = {
+  const homePeriodScore = {
     'period_value': period1Value,
     'score': homeScore1
   };
-  let awayPeriodScore = {
+  const awayPeriodScore = {
     'period_value': period1Value,
     'score': awayScore1
   };
-  let expectedPeriodScore = new PeriodScore({
+  const expectedPeriodScore = new PeriodScore({
     period: period1,
     score: score1
   });
 
-  let homePeriodScores = {
+  const homePeriodScores = {
     'linescores': {
       'period': [
         homePeriodScore,
@@ -52,7 +52,7 @@ describe('translate game scoreboards', function() {
       ]
     }
   };
-  let awayPeriodScores = {
+  const awayPeriodScores = {
     'linescores': {
       'period': [
         awayPeriodScore,
@@ -60,41 +60,75 @@ describe('translate game scoreboards', function() {
       ]
     }
   }
-  let expectedPeriodScores = List.of(
+  const expectedPeriodScores = List.of(
     expectedPeriodScore,
     expectedPeriodScore
   );
 
-  let teamCity = 'Boston';
-  let teamNickname = 'Celtics';
-  let teamAbbreviation = 'BOS';
+  const teamCity = 'Boston';
+  const teamNickname = 'Celtics';
+  const teamAbbreviation = 'BOS';
 
-  let team = {
+  const team = {
     'city': teamCity,
     'nickname': teamNickname,
     'abbreviation': teamAbbreviation
   };
-  let expectedTeam = new Team({
+  const expectedTeam = new Team({
     city: teamCity,
     nickname: teamNickname,
     abbreviation: teamAbbreviation
   });
 
-  let broadcastScope = 'scope';
-  let broadcastDisplayName = 'displayName';
-  let broadcastMedium = BroadcastMedium.TV;
-  let broadcast = {
+  const broadcastScope = 'scope';
+  const broadcastDisplayName = 'displayName';
+  const broadcastMedium = BroadcastMedium.TV;
+  const broadcast = {
     'scope': broadcastScope,
     'display_name': broadcastDisplayName
   };
-  let expectedBroadcast = new Broadcast({
+  const expectedBroadcast = new Broadcast({
     scope: broadcastScope,
     name: broadcastDisplayName,
     medium: broadcastMedium
   });
 
   it('should test period scores parsing', function() {
-    let translatedPeriodScores = GameScoreboardTranslator.getPeriodScores(homePeriodScores, awayPeriodScores);
-    expect(translatedPeriodScores).to.eql(expectedPeriodScores);
+    const translatedPeriodScores = GameScoreboardTranslator.getPeriodScores(homePeriodScores, awayPeriodScores);
+    expect(expectedPeriodScores).to.eql(translatedPeriodScores);
+  });
+
+  it('should test getting broadcasts', () => {
+    const scope = 'scope';
+    const displayName = 'displayName';
+    const broadcast = {
+      'scope': scope,
+      'display_name': displayName
+    };
+    const broadcasters = {
+      radio: {
+        broadcaster: [
+          broadcast,
+        ],
+      },
+    tv: {
+      broadcaster: [
+        broadcast,
+      ],
+    },
+  };
+  const expected = List.of(
+    new Broadcast({
+      scope: scope,
+      name: displayName,
+      medium: BroadcastMedium.TV,
+    }),
+    new Broadcast({
+      scope: scope,
+      name: displayName,
+      medium: BroadcastMedium.RADIO,
+    }),
+  );
+  expect(expected).to.eql(GameScoreboardTranslator.getBroadcasts(broadcasters));
   });
 });
