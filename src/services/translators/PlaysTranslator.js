f8188defd8d7fb30b1537dc9c8f222e4e80e1e56
jaebradley <jae.b.bradley@gmail.com>
2017-09-08 13:57:49 -0500

    update plays translator

--- src/services/translators/PlaysTranslator.js
+++ src/services/translators/PlaysTranslator.js
@@ -5,12 +5,12 @@ import Play from '../../data/Play';
 export default class PlaysTranslator {
   static translate(data) {
     // At most, use the last 5 plays
-    const recentPlays = data.sports_content.game.play.slice(-Math.min(5, plays.length));
+    const recentPlays = data.sports_content.game.play.slice(-Math.min(5, data.length));
     return List(recentPlays.map(play => new Play({
       description: play.description,
       clock: play.clock,
       period: parseInt(play.period),
-      teamAbbreviation: play.team_abr
+      teamAbbreviation: play.team_abr,
     })));
   }
 }
