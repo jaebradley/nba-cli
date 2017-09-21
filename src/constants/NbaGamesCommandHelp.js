777d70c30b1c2df9349cae803b381b5a315e4ddf
jaebradley <jae.b.bradley@gmail.com>
2017-09-21 17:00:58 -0400

    fix linting for nba games command help

--- src/constants/NbaGamesCommandHelp.js
+++ src/constants/NbaGamesCommandHelp.js
@@ -1,7 +1,8 @@
-'use es6';
-
-let USAGE = '\nUsage: nba [options] [commands] \n\n';
-let COMMANDS = 'Commands: \n\n  games [time] get nba games for date \n\n';
-let OPTIONS = 'Options: \n\n  -h, --help  output usage information \n\n';
-let EXAMPLES = 'Examples: \n\n nba games today => get games that started today \n\n nba games tomorrow => get games starting tomorrow \n\n nba games yesterday => get games that started yesterday \n\n nba games 2016-01-01 => nba games that started on 2016-01-01 \n';
-export let HELP = `${USAGE}${COMMANDS}${OPTIONS}${EXAMPLES}`;
+const USAGE = '\nUsage: nba [options] [commands] \n\n';
+const COMMANDS = 'Commands: \n\n  games [time] get nba games for date \n\n';
+const OPTIONS = 'Options: \n\n  -h, --help  output usage information \n\n';
+const EXAMPLES = 'Examples: \n\n nba games today => get games that started today \n\n nba games tomorrow => get games starting tomorrow \n\n nba games yesterday => get games that started yesterday \n\n nba games 2016-01-01 => nba games that started on 2016-01-01 \n';
+const HELP = `${USAGE}${COMMANDS}${OPTIONS}${EXAMPLES}`;
+export {
+  HELP as default,
+};
