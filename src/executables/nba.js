fc32732f6c5c7ae24cafc061b2479cf30d75ec8f
jaebradley <jae.b.bradley@gmail.com>
2017-09-22 18:46:07 -0400

    linting for executables

--- src/executables/nba.js
+++ src/executables/nba.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 
-'use es6';
+/* eslint no-console: 0 */
 
-import program from "commander";
+import program from 'commander';
 
-import {HELP} from "../constants/NbaGamesCommandHelp";
+import { HELP } from '../constants/NbaGamesCommandHelp';
 
-program.version("0.0.1");
+program.version('0.0.1');
 
-program.on('help', function() {
-    console.log(HELP);
-    process.exit(1);
+program.on('help', () => {
+  console.log(HELP);
+  process.exit(1);
 });
 
-program.command("games", "get nba games")
-       .parse(process.argv);
+program.command('games', 'get nba games')
+  .parse(process.argv);
 
 if (program.args.length === 0) {
   console.log(HELP);
