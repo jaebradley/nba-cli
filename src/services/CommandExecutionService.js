a0afb87b72869322123c61223050905ef231f46c
jaebradley <jae.b.bradley@gmail.com>
2017-09-28 00:47:14 -0400

    linting for command executing service

--- src/services/CommandExecutionService.js
+++ src/services/CommandExecutionService.js
@@ -1,19 +1,22 @@
+/* eslint no-console: 0 */
+
 import StartOfDayIdentifier from './StartOfDayIdentifier';
 import DataAggregator from './DataAggregator';
 import TableCreator from '../services/tables/TableCreator';
 
 export default class CommandExecutionService {
   static executeGamesCommand(dateValue) {
-    return DataAggregator.aggregate(StartOfDayIdentifier.identify(dateValue))
-                         .then((data) => TableCreator.create(data))
-                         .then((tables) => CommandExecutionService.printTables(tables));
+    return DataAggregator
+      .aggregate(StartOfDayIdentifier.identify(dateValue))
+      .then(data => TableCreator.create(data))
+      .then(tables => CommandExecutionService.printTables(tables));
   }
 
   static printTables(tables) {
-    if (tables.started.size > 0) {
-      tables.started.forEach((table) => console.log(table));
-    } else {
+    if (tables.started.empty) {
       console.log('No started games');
+    } else {
+      tables.started.forEach(table => console.log(table));
     }
 
     if (tables.upcoming) {
